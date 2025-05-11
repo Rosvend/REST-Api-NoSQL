392ebbcf578925b756330129b818240b8745757f
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2025-05-10 22:48:41 -0500

    added mongodb atlas connection script

---
+++ ping_db.py
@@ -0,0 +1,11 @@
+import asyncio
+from app.db.mongo import client
+
+async def ping():
+    try:
+        await client.admin.command("ping")
+        print("connected to MongoDB successfully!")
+    except Exception as e:
+        print("damn failed to connect:", e)
+
+asyncio.run(ping())
